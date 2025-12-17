d8214bc170b8796ecb353233d4eb17e04c558339
karishma-fariha <karishmafarihakathi10@gmail.com>
2025-12-17 10:23:12 +0600

    added navbar and make it responsive

---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../assets/icons8-blood-64.png';
+
+const Logo = () => {
+    return (
+        <div className='flex items-center space-x-2'>
+            <img className='w-12 h-12' src={logo} alt="" />
+            <div className="leading-none flex flex-col items-start font-bold text-2xl ">
+                <span className='text-red-700'>Blood</span>
+                <span>Donation</span>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Logo;
