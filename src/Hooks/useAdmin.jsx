7cd22b6f51f75916549cd5c1ca9a65777fb4e9ba
karishma-fariha <karishmafarihakathi10@gmail.com>
2025-12-21 15:26:30 +0600

    added admin route and use admin hook

---
+++ src/Hooks/useAdmin.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { AuthContext } from "../Provider/AuthContext";
+
+const useAdmin = () => {
+    const { user, loading } = useContext(AuthContext);
+
+    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        enabled: !loading && !!user?.email, // Only run the query if user is logged in
+        queryFn: async () => {
+            const res = await axios.get(`http://localhost:5000/users/admin/${user.email}`);
+            return res.data?.admin;
+        }
+    });
+
+    return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,18 +1,23 @@
-import React, { use } from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthContext';
+import useAdmin from '../Hooks/useAdmin';
 import Loading from '../Pages/Loading';
 
-const AdminRoute = () => {
-    const {user,loading} =use(AuthContext);
-    
-    if(loading) {
+const AdminRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
         return <Loading></Loading>
     }
-    return (
-        <div>
-            
-        </div>
-    );
+
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>;
 };
 
 export default AdminRoute;
